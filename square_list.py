e851a196deac6162bf081bb31b46039b5317ec30
kimberlyroj <90664797+kimberlyroj@users.noreply.github.com>
2022-02-18 21:53:12 -0800

    second push

--- square_list.py
+++ square_list.py
@@ -1,3 +1,7 @@
+# Author: Kimberly Rojas
+# GitHub username: kimberlyroj
+# Date: 2/18/2022
+# Description: A function named square_list that takes as a parameter a list of numbers and replaces each value with the square of that value
 def square_list(lst):
     for i in range(len(lst)):
         lst[i] = lst[i] * lst[i]
